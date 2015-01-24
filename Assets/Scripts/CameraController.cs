3f44f7d8ef85d2c88e1039f9ed2e5b90c14d289a
Veli-Pekka Kokkonen <veli@unity3d.com>
2015-01-24 18:46:37 +0200

    move camera to right

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,11 +3,13 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour
 {
+	public float speed;
 	public Transform followTarget;
 
     Vector2 vel;
 	void Update()
 	{
+		/*
 		Vector3 target = Vector2.zero;
 		foreach (Transform child in followTarget)
 			target += child.position;
@@ -16,6 +18,9 @@ public class CameraController : MonoBehaviour
 
         Vector2 pos = Vector2.SmoothDamp(transform.position, target, ref vel, .2f);
         transform.position = new Vector3(pos.x, pos.y, transform.position.z);
+		 */
+
+		transform.Translate (Vector3.right * speed * Time.deltaTime);
     }
 
 }
