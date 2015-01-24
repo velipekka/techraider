e85f51113ee60c584805b5b2dc27aad93b1603b4
J-dawg <juuso.mattila@gmail.com>
2015-01-24 16:03:20 +0200

    lazor

---
+++ Assets/Scripts/LaserVisuals.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserVisuals : MonoBehaviour {
+
+    public ParticleSystem chargeParticle;
+
+    void Update()
+    {
+        chargeParticle.emissionRate = GetComponent<Animator>().GetFloat("charge") * 200f;
+    }
+
+}
---
+++ Assets/Scripts/WorldMachine.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class WorldMachine : MonoBehaviour 
+{
+    public List<GameObject> obstaclePrefabs;
+    public List<GameObject> obstacleInstances;
+
+    public int propCountMin = 10;
+    public int propCountMax = 10;
+
+    public Rect propBounds;
+
+    void Start()
+    {
+        obstacleInstances = new List<GameObject>();
+        MakeWorld();
+    }
+
+    public void MakeWorld()
+    {
+        foreach(GameObject go in obstacleInstances)
+        {
+            Destroy(go.gameObject);
+        }
+
+        obstacleInstances = new List<GameObject>();
+
+        int propCount = Random.Range(propCountMin, propCountMax);
+
+        for (int i = 0; i < propCount; i++)
+        {
+            GameObject prefab = obstaclePrefabs[Random.Range(0, obstaclePrefabs.Count)];
+
+           
+            bool clear = false;
+            Vector2 pos = Vector2.zero;
+            while (!clear)
+            {
+                pos = new Vector2(Random.Range(propBounds.x, propBounds.x + propBounds.width), Random.Range(propBounds.y, propBounds.height));
+
+                Collider2D c = Physics2D.OverlapPoint(pos, 1 << LayerMask.NameToLayer("obstacles"));
+
+                if(c == null)
+                {
+                    clear = true;
+                }
+                else
+                {
+                }
+
+            }
+
+            GameObject instance = Instantiate(prefab, pos, prefab.transform.rotation) as GameObject;
+            obstacleInstances.Add(instance);
+
+        }
+
+    }
+
+	
+}
