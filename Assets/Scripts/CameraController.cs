b610d403897983f9af9817bf81081bfb177872b3
Veli-Pekka Kokkonen <veli@unity3d.com>
2015-01-24 18:30:59 +0200

    umm.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,14 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraController : MonoBehaviour {
-
-    public ShipGenerator shipGenerator;
+public class CameraController : MonoBehaviour
+{
+	public Transform followTarget;
 
     Vector2 vel;
 	void Update()
     {
-        Vector2 target = shipGenerator.slots[0].gameObject.transform.position;
+        Vector2 target = followTarget.position;
 
         Vector2 pos = Vector2.SmoothDamp(transform.position, target, ref vel, .2f);
         transform.position = new Vector3(pos.x, pos.y, transform.position.z);
