afd4287224871f7d4e678ece73eb4591ef3aea10
Veli-Pekka Kokkonen <veli@unity3d.com>
2015-01-24 18:53:48 +0200

    Game Over when player exits the screen

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using RageEvent;
+using UnityEngine;
 using System.Collections;
 
 public class CameraController : MonoBehaviour
@@ -9,18 +10,22 @@ public class CameraController : MonoBehaviour
     Vector2 vel;
 	void Update()
 	{
-		/*
 		Vector3 target = Vector2.zero;
 		foreach (Transform child in followTarget)
 			target += child.position;
 
         target /= followTarget.childCount;
 
-        Vector2 pos = Vector2.SmoothDamp(transform.position, target, ref vel, .2f);
-        transform.position = new Vector3(pos.x, pos.y, transform.position.z);
-		 */
+		//Vector2 pos = Vector2.SmoothDamp(transform.position, target, ref vel, .2f);
+		//transform.position = new Vector3(pos.x, pos.y, transform.position.z);
 
 		transform.Translate (Vector3.right * speed * Time.deltaTime);
-    }
+
+		float padding = 0.1f;
+		Rect viewRect = new Rect(-padding, -padding, 1 + padding, 1 + padding);
+		Vector2 viewPosition = camera.WorldToViewportPoint(target);
+		if (!viewRect.Contains(viewPosition))
+			EventManager.Trigger("GameOver");
+	}
 
 }
