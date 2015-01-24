9dd941f2250ae66e174d3132c01165f0cd584e89
Veli-Pekka Kokkonen <veli@unity3d.com>
2015-01-24 22:12:41 +0200

    stuff

---
+++ Assets/Meteor.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Serialization;
+using UnityEngine;
+using System.Collections;
+
+public class Meteor : MonoBehaviour
+{
+	IEnumerator Start()
+	{
+		rigidbody2D.velocity = Vector2.right * -3;
+
+		yield return new WaitForSeconds(20);
+		Destroy(gameObject);
+	}
+}
