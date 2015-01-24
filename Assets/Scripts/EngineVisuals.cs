511a8e7f9f41a17cfeec5bf2e20532856cb12a9b
Veli-Pekka Kokkonen <veli@unity3d.com>
2015-01-24 14:34:41 +0200

    cleaning up

---
+++ Assets/Scripts/EngineVisuals.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EngineVisuals : MonoBehaviour {
+
+	public ParticleSystem particleSystem;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		particleSystem.emissionRate = GetComponent<Animator> ().GetFloat ("power") * 100f;
+		particleSystem.startSpeed = GetComponent<Animator> ().GetFloat ("power") * 5f;
+		particleSystem.startLifetime = .25f + GetComponent<Animator> ().GetFloat ("power") * .75f;
+	}
+}
